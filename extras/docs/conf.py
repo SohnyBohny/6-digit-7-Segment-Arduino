6185a14f5240c2cb11c9af532a49ad6e6db4de83
SohnyBohny <sohny.bean@streber24.de>
2016-04-08 17:11:19 +0200

    add docu

---
+++ extras/docs/conf.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import shlex
+import subprocess
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    subprocess.call('doxygen', shell=True)
+
+extensions = ['breathe']
+breathe_projects = { '6-digit-7-Segment-Arduino': 'xml' }
+breathe_default_project = "6-digit-7-Segment-Arduino"
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+project = u'6-digit-7-Segment-Arduino'
+copyright = u'2015, 6-digit-7-Segment-Arduino'
+author = u'6-digit-7-Segment-Arduino'
+version = '1.0'
+release = '1.0'
+language = None
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_static_path = ['_static']
+htmlhelp_basename = '6-digit-7-Segment-Arduinodoc'
+latex_elements = {
+}
+latex_documents = [
+  (master_doc, '6-digit-7-Segment-Arduino.tex', u'6-digit-7-Segment-Arduino Documentation',
+   u'6-digit-7-Segment-Arduino', 'manual'),
+]
---
+++ extras/docs/index.rst
@@ -0,0 +1,18 @@
+.. toctree::
+   :maxdepth: 2
+
+.. include:: ../../README.rst
+   :end-before: ----
+
+Class Documentation
+===================
+
+.. doxygenclass:: 6-digit-7-Segment-Arduino
+   :project: 6-digit-7-Segment-Arduino
+   :members:
+
+----
+
+This documentation was built using ArduinoDocs_.
+
+.. _ArduinoDocs: http://arduinodocs.readthedocs.org
